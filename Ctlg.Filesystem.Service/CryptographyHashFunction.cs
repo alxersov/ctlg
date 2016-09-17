b2bdbe60fd16379559b2462b9e6f41739c2a504e
Alexei Ersov <13179193+alxersov@users.noreply.github.com>
2016-09-17 21:33:40 +0300

    Add support for SHA-384, SHA-512, MD5

---
+++ Ctlg.Filesystem.Service/CryptographyHashFunction.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Ctlg.Filesystem.Service
+{
+    /// <summary>
+    /// Adapter for <see cref="System.Security.Cryptography.HashAlgorithm"/>. Allows to use any HashAlgorithm as IHashFunction.
+    /// </summary>
+    public class CryptographyHashFunction<THashAlgorithm> : IHashFunction where THashAlgorithm : HashAlgorithm
+    {
+        public CryptographyHashFunction(THashAlgorithm algorithm, string name)
+        {
+            Algorithm = algorithm;
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public byte[] CalculateHash(Stream stream)
+        {
+            return Algorithm.ComputeHash(stream);
+        }
+
+        private readonly THashAlgorithm Algorithm;
+    }
+}
