bfd744ee32280bd14ad3b7a05c09434efd6ebade
Alexei Ersov <13179193+alxersov@users.noreply.github.com>
2019-10-18 19:03:31 +0300

    Add logging with NLog

---
+++ Ctlg/EventHandlers/Logging.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ctlg.Service;
+using Ctlg.Service.Events;
+
+namespace Ctlg.EventHandlers
+{
+    public class Logging : IHandle<ErrorEvent>, IHandle<Warning>
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public void Handle(ErrorEvent args)
+        {
+            Logger.Error(args.Exception, args.Message);
+        }
+
+        public void Handle(Warning args)
+        {
+            Logger.Warn(args.Message);
+        }
+    }
+}
